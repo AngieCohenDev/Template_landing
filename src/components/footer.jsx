3782b83806ce1846b042641af4cf099cd84f7934
Angie Cohen <angiedev2001@gmail.com>
2024-08-20 17:15:43 -0300

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2024 Claro Argentina.    
+            <a href='http://www.templatewire.com' rel='nofollow'>
+                Todos los derechos reservados
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
